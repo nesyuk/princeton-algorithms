93bfe67587e345e2df27de98b13c6427bc90c1ce
Nelly Kuznetsova <nelly@fwd.be>
2021-10-02 16:17:53 +0200

    Refactor

--- graph/bfs.go
+++ graph/bfs.go
@@ -1,26 +1,24 @@
 package graph
 
-
-func Dfs(node *Node, target int) bool {
+func Bfs(node *Node, target int) bool {
 	if node == nil {
 		return false
 	}
 	visited := map[*Node]bool{}
-	stack := []*Node{node}
-	for len(stack) > 0 {
-		current := stack[len(stack)-1]
-		stack = stack[:len(stack)-1]
+	queue := []*Node{node}
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
 		visited[current] = true
 		if current.Val == target {
 			return true
 		}
 		for _, neighbour := range current.Neighbours {
 			if !visited[neighbour] {
-				stack = append(stack, neighbour)
+				queue = append(queue, neighbour)
 			}
 		}
 	}
-	
+
 	return false
 }
-
--- graph/bfs_test.go
+++ graph/bfs_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 
-func TestDfs(t *testing.T) {
+func TestBfs(t *testing.T) {
 	root := NewDiGraph(map[int][]int{1: []int{2, 3}, 2: []int{3}, 4: []int{4}}, 1)
 	tests := []struct{data int; want bool}{
 		{4, false},
@@ -13,14 +13,14 @@ func TestDfs(t *testing.T) {
 		{2, true},
 	}
 	for _, test := range tests {
-		isConnected := Dfs(root, test.data)
+		isConnected := Bfs(root, test.data)
 		if isConnected != test.want {
 			t.Fatalf("root: %v to node: %v, expected: %v, got: %v", root.Val, test.data, test.want, isConnected)
 		}
 	}
 }
 
-func TestDfsWithLoops(t *testing.T) {
+func TestBfsWithLoops(t *testing.T) {
 	root := NewDiGraph(map[int][]int{1: []int{2, 3}, 2: []int{1, 3}, 4: []int{4}}, 1)
 	tests := []struct{data int; want bool}{
 		{4, false},
@@ -28,10 +28,9 @@ func TestDfsWithLoops(t *testing.T) {
 		{2, true},
 	}
 	for _, test := range tests {
-		isConnected := Dfs(root, test.data)
+		isConnected := Bfs(root, test.data)
 		if isConnected != test.want {
 			t.Fatalf("root: %v to node: %v, expected: %v, got: %v", root.Val, test.data, test.want, isConnected)
 		}
 	}
 }
-
